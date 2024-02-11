315e4b805347af6ef67328a995c81be990fa6e40
JackBraunYKT <aa.ivanov1994@gmail.com>
2024-02-11 16:54:52 +0900

    fix fetchData

--- js/fetchData.js
+++ js/fetchData.js
@@ -1,6 +1,6 @@
 export async function fetchData(url) {
   return fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => data)
     .catch((error) => console.log(error));
 }
